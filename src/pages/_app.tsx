5350d913919d814459e8fa204637f8aee13fef17
Adrian Hideki <adrian.hideki.br@gmail.com>
2021-05-24 20:39:36 -0300

    Adicionando estilização padrão e fontes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    // o resetCSS remove as estilizações padrão do html element
+    // ex: margin no body
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>)
 }
 
 export default MyApp
