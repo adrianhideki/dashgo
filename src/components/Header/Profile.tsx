7d2960e0304a5b3e8f3e70dfbd654faa02676db6
Adrian Hideki <adrian.hideki.br@gmail.com>
2021-05-25 21:49:08 -0300

    Adicionando responsividade no header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,21 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
-export default function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export default function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Adrian Hideki</Text>
-        <Text color="gray.300" fontSize="small">
-          adrian.hideki.br@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Adrian Hideki</Text>
+          <Text color="gray.300" fontSize="small">
+            adrian.hideki.br@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
