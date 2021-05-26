b8fd4966250b2c7cb329dc3ee7f149da4216691a
Adrian Hideki <adrian.hideki.br@gmail.com>
2021-05-25 22:41:57 -0300

    Adicionando ActiveLink

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,18 +1,24 @@
 import React, { ElementType } from "react";
-import { Icon, Text, Link, LinkProps } from "@chakra-ui/react";
+import { Icon, Text, Link as ChakraLink, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
+import ActiveLink from "../ActiveLink";
 
-interface NavLinkProps extends LinkProps {
+interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType; // usando quando se passa um nome e não o componente direto
   children: string;
+  href: string;
 }
 
-export default function NavLink({ icon, children, ...props } : NavLinkProps) {
+export default function NavLink({ icon, children, href, ...props }: NavLinkProps) {
   return (
-    <Link {...props} display="flex" align="center">
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWeight="medium">
-        {children}
-      </Text>
-    </Link>
+    // o passHref gera o efeito do link ao passar o moused
+    // utilizar quando n se tem uma tag a dentro do Link do next
+    <ActiveLink href={href} passHref> 
+      <ChakraLink {...props} display="flex" align="center">
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWeight="medium">
+          {children}
+        </Text>
+      </ChakraLink>
+    </ActiveLink>
   );
 }
