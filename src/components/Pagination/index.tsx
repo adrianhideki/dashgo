12518c7e4a99daa0c28c9bf0d4db6ff1c5a96cdc
Adrian Hideki <adrian.hideki.br@gmail.com>
2021-05-29 11:58:13 -0300

    Complementando componente de paginação

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,13 +1,48 @@
-import { Button } from "@chakra-ui/button";
-import { Stack } from "@chakra-ui/layout";
-import { Box } from "@chakra-ui/react";
 import React from "react";
+import { Button, Box, Text, Stack } from "@chakra-ui/react";
 import PaginationItem from "./PaginationItem";
 
-export function Pagination() {
+interface PaginationProps {
+  totalCountOfRegisters: number;
+  registersPerPage?: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+}
+
+const sibilingsCount = 1;
+
+function generatePagesArray(from: number, to: number) {
+  return [...new Array(to - from)]
+    .map((_, index) => {
+      return from + index + 1;
+    })
+    .filter((page) => page > 0);
+}
+
+export function Pagination({
+  totalCountOfRegisters,
+  onPageChange,
+  currentPage = 1,
+  registersPerPage = 10,
+}: PaginationProps) {
+  const lastPage = Math.floor(totalCountOfRegisters / registersPerPage);
+
+  const previousPages =
+    currentPage > 1
+      ? generatePagesArray(currentPage - 1 - sibilingsCount, currentPage - 1)
+      : [];
+
+  const nextPages =
+    currentPage < lastPage
+      ? generatePagesArray(
+          currentPage,
+          Math.min(currentPage + sibilingsCount, lastPage)
+        )
+      : [];
+
   return (
     <Stack
-      direction={["column","row"]}
+      direction={["column", "row"]}
       mt="8"
       justify="space-between"
       align="center"
@@ -17,12 +52,33 @@ export function Pagination() {
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
       <Stack direction="row" spacing="2">
-        <PaginationItem number={1} isCurrent/>
-        <PaginationItem number={2} />
-        <PaginationItem number={3} />
-        <PaginationItem number={4} />
-        <PaginationItem number={5} />
-        <PaginationItem number={6} />
+        {currentPage > 1 + sibilingsCount && (
+          <>
+            <PaginationItem number={1} />
+            {currentPage > 2 + sibilingsCount && (
+              <Text color="gray.300" width="8" textAlign="center">
+                ...
+              </Text>
+            )}
+          </>
+        )}
+        {previousPages.length > 0 &&
+          previousPages.map((page) => (
+            <PaginationItem number={page} key={page} />
+          ))}
+        <PaginationItem number={currentPage} isCurrent />
+        {nextPages.length > 0 &&
+          nextPages.map((page) => <PaginationItem number={page} key={page} />)}
+        {currentPage + sibilingsCount < lastPage && (
+          <>
+            {currentPage + 1 + sibilingsCount < lastPage && (
+              <Text color="gray.300" width="8" textAlign="center">
+                ...
+              </Text>
+            )}
+            <PaginationItem number={lastPage} />
+          </>
+        )}
       </Stack>
     </Stack>
   );
