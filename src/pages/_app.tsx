22368658e091a38cc373566a4c4fb3798168acff
Adrian Hideki <adrian.hideki.br@gmail.com>
2021-05-29 11:03:06 -0300

    Adicionando ReactQuery Dev tools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "../styles/theme";
 import SidebarDrawerProvider from "../contexts/SidebarDrawerContent";
 import { makeServer } from "../services/mirage";
@@ -22,6 +23,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </SidebarDrawerProvider>
       </ChakraProvider>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
