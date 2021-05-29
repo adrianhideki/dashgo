03a404b956667c3fcc9373a4d97ef64bafd3167c
Adrian Hideki <adrian.hideki.br@gmail.com>
2021-05-29 10:20:53 -0300

    Adicionando Factory no mirage e instalando o faker

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,5 @@
-import { createServer, Model } from "miragejs";
+import faker from "faker";
+import { createServer, Factory, Model } from "miragejs";
 
 type User = {
   name: string;
@@ -11,14 +12,30 @@ export function makeServer() {
     models: {
       user: Model.extend<Partial<User>>({} as User),
     },
+    factories: {
+      user: Factory.extend({
+        name(i: number) {
+          return faker.name.firstName();
+        },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        createdAt() {
+          return faker.date.recent(10);
+        },
+      }),
+    },
+    seeds(server) {
+      server.createList('user', 200);
+    },
     routes() {
-      this.namespace = 'api';
+      this.namespace = "api";
       this.timing = 750; // 750ms
 
-      this.get('/users');
-      this.post('/users');
+      this.get("/users");
+      this.post("/users");
 
-      this.namespace = '';
+      this.namespace = "";
       // todas as chamadas q passarem pelo mirage e n tive uma rota
       // não impede o direcionamento
       this.passthrough();
